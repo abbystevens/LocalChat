4a44a1c774f1ab6d714ddbc85b39a1a1f90268cb
abbystevens <adeats@gmail.com>
2014-11-20 19:10:23 -0800

    Initial Commit

---
+++ LocalChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalChat
+//
+//  Created by Abby Stevens on 11/20/14.
+//  Copyright (c) 2014 Abby Stevens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalChat
+//
+//  Created by Abby Stevens on 11/20/14.
+//  Copyright (c) 2014 Abby Stevens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
